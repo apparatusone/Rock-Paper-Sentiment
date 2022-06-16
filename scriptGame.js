f02205c93f25d38ee52ef9339dac8f79ddf6dde7
Devin Turner <devinturner@me.com>
2022-06-16 19:56:58 +0200

    added several graphics and animations graphics are wip
    added prelimenary game over function
    
    added placeholder buttons for onscreen controls

--- scriptGame.js
+++ scriptGame.js
@@ -1,15 +1,6 @@
-// Pseudocode //
-// Get input from player (rock, paper, or scissors)
-// Store as playerSelection
-// Generate random selection from (rock, paper, or scissors)
-// Store as computerSelection
-// Create rules for game (rock > scissor | scissors > paper | paper > rock)
-// Compare playerSelection to computerSelection
-// Display who won
-
-let textDiv = document.querySelector(".text");
-
 const selectionArray = ['ROCK', 'PAPER', 'SCISSORS'];
+const mainContainer = document.querySelector("#container");
+const graphicsContainer = document.querySelector("#graphicscon");
 let result = 'default';
 let win = 'You Win!';
 let lose = 'You Lose';
@@ -53,6 +44,8 @@ let player = '';
 let resultGame = '';
 let computer = '';
 
+// primary game function, sets computer selection, player selection, determines winner (or tie)
+
 function mainGame() {
     computer = computerSelection();
     gameRules(player, computer);
@@ -69,12 +62,15 @@ function mainGame() {
 
     console.log("Player Score:",playScore, "Computer Score:",compScore);
 
-    if (compScore === 0) {
-        console.log("GAME OVER", win);
-    } else if (playScore ===0) {
-        console.log("GAME OVER", lose);
-    }
+    healthBar();
+}
 
+function resetGame() {
+    playScore = 5;
+    compScore = 5;
+    clearText();
+    hideElements("#rps, .rpscon");
+    unHideElements("#fightmenu, #fs");
     healthBar();
 }
 
@@ -89,10 +85,8 @@ currentSelection = "x1y2 main"
 window.addEventListener('keydown', keyPress);
 
 locked = false //lock keyboard during a setTimeout, default is unlocked
-function lockKeyboard (time) {
-    locked = true;
-    setTimeout(() => locked = false, time);
-}
+
+// keyboard navigation of game
 
 function keyPress(e) {
     if( locked ){
@@ -120,10 +114,12 @@ function keyPress(e) {
             move(5);
             arrow();
             selectionMenu(curSel.menu);
+            console.log("test");
         break;
         case 66: // B
             if (curSel.menu === "rps") {
-                hideMenus()
+                hideElements("#rps, .rpscon");
+                unHideElements("#fightmenu, #fs")
                 curSel.menu = "main";
                 curSel.x = 1;
                 curSel.y = 2;
@@ -135,6 +131,8 @@ function keyPress(e) {
     }
     e.preventDefault();
 };
+
+// navigates menus of arbitrary x/y dimensions using class names
 
 function navFunc(curSel, x, y) { //generates class to navigate
     let menu = document.querySelectorAll(`.${curSel.menu}`);   //get items in current menu to find max array dimensions
@@ -173,6 +171,8 @@ function move(direction){
     }
 }
 
+// menu selector
+
 function arrow() {
     let menu = document.querySelectorAll(`.${curSel.menu}`);
     menu.forEach(element => element.classList.add('hideelement'));
@@ -182,22 +182,28 @@ function arrow() {
     currentSelection = `x${curSel.x}y${curSel.y} ${curSel.menu}`;
 };
 
-function hideMenus() {
-    let hide = document.querySelectorAll(".rpsselection");
+// hide / unhide menus
+
+function hideElements(...args) {
+    let hide = document.querySelectorAll(...args);
     hide.forEach(element => element.classList.add('hideelement'));
 }
 
-function hideMenusAll() {
-    let hide = document.querySelectorAll(".rpscon, #fightmenu, #fs, #rps");
-    hide.forEach(element => element.classList.add('hideelement'));
+function unHideElements(...args) {
+    let hide = document.querySelectorAll(...args);
+    hide.forEach(element => element.classList.remove('hideelement'));
 }
 
-function unHideMenus() {
-    let unhiderps1 = document.querySelectorAll(".rpscon, #rps");
-    unhiderps1.forEach(element => element.classList.remove('hideelement'));
-
-    let unhide2 = document.querySelectorAll(".fightmenu");
-    unhide2.forEach(element => element.classList.remove('hideelement'));
+async function hide(text){
+    await typeText(text);
+    setTimeout(() => {
+        unHideElements(".rpscon, #rps, .fightmenu");
+        clearText();
+        locked = false;
+    }, 1000);
+    setTimeout(() => {
+        gameOver()
+    }, 4000);
 }
 
 function selectionMenu(menuItem) {
@@ -209,14 +215,41 @@ function selectionMenu(menuItem) {
             selectionItem(currentSelection);
         break;
     }
-
 }
+
+function gameOver() {
+    if (compScore === 0) {
+        hideElements(".rpscon, #fightmenu, #fs, #rps");
+        typeText("YOU WIN!");
+        setTimeout(() => {
+            resetGame()
+        }, 2000);
+        return true;
+    } else if (playScore === 0) {
+        hideElements(".rpscon, #fightmenu, #fs, #rps");
+        typeText("YOU LOSE!");
+        setTimeout(() => {
+            resetGame()
+        }, 2000);
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function gameSequence() {
+    mainGame(); 
+    setTimeout(hideElements(".rpscon, #fightmenu, #fs, #rps"), 100); //hide all menus
+    locked = true;
+    hide(gameText(resultGame, player, computer));
+    player = ""; //clear player selection
+};
 
 function selectionItem(item) {
     switch(item) {
         case "x1y2 main":
-            hideMenusAll();
-            unHideMenus();
+            hideElements(".rpscon, #fightmenu, #fs, #rps");
+            unHideElements(".rpscon, #rps, .fightmenu")
             curSel.menu = "rps";
             curSel.x = 1;
             curSel.y = 4;
@@ -231,78 +264,67 @@ function selectionItem(item) {
         break;
         case "x1y4 rps":
             player = "ROCK";
-            mainGame(); // delay testing
-            setTimeout(hideMenusAll, 100);
-            gameText(resultGame, player, computer)
-            player = ""; //reset player
-            lockKeyboard(3000);
-            setTimeout(unHideMenus, 3000);
-            setTimeout(hideText, 3000);
+            gameSequence();
         break;
         case "x1y3 rps":
             player = "PAPER";
-            mainGame(); // delay testing
-            setTimeout(hideMenusAll, 100);
-            gameText(resultGame, player, computer)
-            player = ""; //reset player
-            lockKeyboard(3000);
-            setTimeout(unHideMenus, 3000);
-            setTimeout(hideText, 3000);
+            gameSequence();
         break;
         case "x1y2 rps":
             player = "SCISSORS";
-            mainGame(); // delay testing
-            setTimeout(hideMenusAll, 100);
-            gameText(resultGame, player, computer)
-            player = ""; //reset player
-            lockKeyboard(3000);
-            setTimeout(unHideMenus, 3000);
-            setTimeout(hideText, 3000);
+            gameSequence();
         break;
     }
 
 }
 
-function hideText() {
-    textDiv.textContent = "";
-}
+let textDiv = document.querySelector(".text");
 
-let interval = 40;
+let clearText = () => textDiv.textContent = "";
+
+let interval = 50;
 
 function typeText(text) {
-    let i = 0;
-    function myLoop() { 
-      setTimeout(function() {
-        let char = document.createTextNode(text[i]);
-        if (text[i] === "#") {
-            linebreak = document.createElement("br");
-            textDiv.appendChild(linebreak);
-        } else {
-            textDiv.appendChild(char);
-        };
-        i++;
-        if (i < text.length) {   //  if the counter < string length, call the loop function
-          myLoop();
-        }                       
-      }, interval)                    // set interval
-    }
-    myLoop();
-    return;
+    return new Promise((resolve) => {
+        let i = 0;
+        function myLoop() { 
+            setTimeout(function() {
+                let char = document.createTextNode(text[i]);
+                if (text[i] === "#") {
+                linebreak = document.createElement("br");
+                textDiv.appendChild(linebreak);
+                space = document.createElement("span"); //moves next line over 2 'pixels'
+                space.style.cssText = "margin-left: .25em;"
+                textDiv.appendChild(space);
+            } else {
+                textDiv.appendChild(char);
+            };
+            i++;
+            if (i === text.length) {
+                resolve("done");
+            }
+            if (i < text.length) {   //  if the counter < string length, call the loop function
+                myLoop();
+            }                       
+        }, interval)               // set interval
+        }
+        myLoop();
+    })
 }
 
 function gameText(result, player, computer) {
-    console.log(result);
-    switch(result) {
-        case "win":
-            typeText(`TRAINER## used ${computer}`);
-        break;
-        case "lose":
-            typeText(`TRAINER## used ${computer}`);
-        break;
-        case "tie":
-            typeText(`TRAINER## used ${computer}, TIE`);
-        break;
-    }
+        console.log(result);
+        switch(result) {
+            case "win":
+                return `PATROCLUS## used ${computer}!`;
+            //break;
+            case "lose":
+                return `PATROCLUS## used ${computer}!`;
+            break;
+            case "tie":
+                return `PATROCLUS## used ${computer}, TIE!`;
+            break;
+        }
 }
 
 function healthBar() {
@@ -314,9 +336,152 @@ function healthBar() {
     playerHealth.style.width = `${playerBar}em`;
 }
 
+//buttons
 
 
+// const upButton = document.querySelector("#upButton");
+// console.log(upButton);
+// upButton.addEventListener("click", move(1));
+// moveUp
+
+document.getElementById("upButton").onclick = function() {(move(2), arrow())};
+document.getElementById("downButton").onclick = function() {(move(4), arrow())};
+document.getElementById("leftButton").onclick = function() {(move(1), arrow())};
+document.getElementById("rightButton").onclick = function() {(move(3), arrow())};
+document.getElementById("aButton").onclick = function() {(
+    move(5),
+    arrow(),
+    selectionMenu(curSel.menu)
+)};
+// document.getElementById("bButton").onclick = function() {()};
+
+// function moveUp() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
 
+// animation stuff
 
+//javaboy splash screen
+const done = "done";
 
+// await end of animation function
+
+async function animationEnd(ele, animationType) {
+    let promise = new Promise((resolve) => {
+        ele.addEventListener(animationType, () => {
+            console.log('finished');
+            resolve("done");
+        });
+      });
+      let result = await promise;
+}
+
+async function javaBoy() {
+    await initialAnimation(done);
+    const colors = ["#FFD757" , "#FF673F" , "#FFB6FB" , "#3ABA42" , "#3293F6"];
+
+    mainContainer.classList.remove('hideelement');
+    mainContainer.classList.add('fadein');
+    
+    function timer(ms) { return new Promise(res => setTimeout(res, ms)); }
+    function delay(ms) { return new Promise(res => setTimeout(res, ms)); }
+
+    async function javaboyReveal() {
+        await delay(500);
+        i = 1;
+        for (const ele of colors) {
+            svg = document.createElement("div");
+            svg.setAttribute("id",`splash${i}`);
+            svg.style.backgroundColor = ele;
+            svg.style.webkitMaskImage = "url(img/javaboy.svg)";
+            svg.style.maskImage = "url(img/javaboy.svg)";
+            svg.style.maskMode = "alpha";
+            svg.style.position = "absolute";
+            svg.style.width = "24em";
+            svg.style.height = "21.6em";
+            graphicsContainer.append(svg);
+            console.log(i);
+            if (i === 5 ) {
+                const animated = document.querySelector('#splash5');
+                await animationEnd(animated, 'animationend');
+            }
+            i++
+            await timer(150);
+        }
+    }
+
+    async function remove() {
+        await javaboyReveal(done);
+        await delay(1);
+        for (let i = 1; i < 5; i++) {
+                let svgR = document.getElementById(`splash${i}`);
+                svgR.remove();
+        }
+    };
+
+    async function fadeout() {
+        await remove(done);
+        await delay (1000);
+        let svgOpacity = document.getElementById('splash5');
+        svgOpacity.classList.add('fadeout');
+        const animated = document.querySelector('.fadeout');
+        console.log(animated);
+        await animationEnd(animated, 'transitionend');
+    }   
+
+    await fadeout();
+    // return new Promise((resolve) => { resolve(done); });
+}
+
+// javaboy reveal
+
+function initialAnimation() {
+    const animContainer = document.querySelector(".handheld");
+    const img = ["pcb", "extras", "case", "screenborder"];
+
+    async function outline(done) {
+        svg = document.createElement("object");
+        svg.classList.add("cls-1");
+        svg.style.position = "absolute";
+        svg.setAttribute("type","image/svg+xml");
+        svg.setAttribute("data","img/outline.svg");
+        animContainer.append(svg);
+        return new Promise(done => setTimeout(done, 1700));
+    }
+
+    async function layers() {
+        await outline(done)
+        i = 1;
+        for (const ele of img) {
+            svg = document.createElement("object");
+            svg.setAttribute("id",`layer${i}`);
+            svg.classList.add("swipereveal");
+            svg.setAttribute("type","image/svg+xml");
+            svg.setAttribute("data",`img/${ele}.svg`);
+            animContainer.append(svg);
+            i++
+            await timer(1000);
+        }
+    }
+
+    // outline();
+    layers();
+
+    function timer(ms) { return new Promise(res => setTimeout(res, ms)); }
+    return new Promise(done => setTimeout(done, 5700));
+}
+
+async function unhidemenus(...args) {
+    await javaBoy(done);
+    unHideElements(...args);
+}
+
+unhidemenus("#main, #fs, #tb, #pb, #cat, #livecon, #fightmenu, #controls");
+
+// case 83: // S
+//     interval = 5;   
+//     console.log
+// break;
+  
+  
